0bc88ff6e1791fc3b97281f27c56bec55120aa89
Soumyadeep1610 <soumyadeepkundu1610@gmail.com>
2024-03-21 00:28:54 +0530

    initial setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+import { Root, Home, Error } from "./routes";
+
+import "./index.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />} errorElement={<Error />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
